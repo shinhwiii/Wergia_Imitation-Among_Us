74e4246443ecafc44367f048dcc1c5995d153ac8
shinhwiii <sh0122451@gmail.com>
2024-01-24 18:58:46 +0900

    메인메뉴 만들기
    메인메뉴 만들기

---
+++ Assets/Scripts/PlayerSettings.cs
@@ -0,0 +1,10 @@
+public enum EControlType
+{
+    Mouse,
+    KeyboardMouse
+}
+
+public class PlayerSettings
+{
+    public static EControlType controlType;
+}
---
+++ Assets/UI/Scripts/MainMenuUI.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class MainMenuUI : MonoBehaviour
+{
+    public void OnClickOnlineButton()
+    {
+        Debug.Log("Click Online");
+    }
+
+    public void OnClickQuitButton()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
